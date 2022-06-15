7efbb75edfb47e9f10e935c326f72a9caa9ca255
Tang Zhilin <tangzhilinnz@gmail.com>
2022-06-15 17:55:38 +0800

    added class Fence in runtime_logger.h

--- runtime_logger.h
+++ runtime_logger.h
@@ -37,7 +37,61 @@
 // Determines the byte size of the per-thread StagingBuffer that decouples
 // the producer logging thread from the consumer background compression
 // thread. This value should be large enough to handle bursts of activity.
-#define STAGING_BUFFER_SIZE    1 << 20;
+#define STAGING_BUFFER_SIZE    1 << 20
+#define BYTES_PER_CACHE_LINE   64
+
+
+/**
+ * This class is used to restrict instruction reordering within a CPU in
+ * order to maintain synchronization properties between threads.  Is a thin
+ * wrapper around x86 "fence" instructions.  Note: getting fencing correct
+ * is extremely tricky!  Whenever possible, use existing solutions that already
+ * handle the fencing.
+ */
+class Fence {
+public:
+
+    /**
+     * This method creates a compiler level memory barrier forcing optimizer
+     * to not re-order memory accesses across the barrier. It can not prevent
+     * CPU reordering.
+     */
+    static void inline barrier() {
+        __asm__ __volatile__("" ::: "memory");
+    }
+
+    /**
+     * This method creates a boundary across which load instructions cannot
+     * migrate: if a memory read comes from code occurring before (after)
+     * invoking this method, the read is guaranteed to complete before (after)
+     * the method is invoked.
+     */
+    static void inline lfence() {
+        __asm__ __volatile__("lfence" ::: "memory");
+    }
+
+    /**
+     * This method creates a boundary across which store instructions cannot
+     * migrate: if a memory store comes from code occurring before (after)
+     * invoking this method, the store is guaranteed to complete before (after)
+     * the method is invoked.
+     */
+    static void inline sfence() {
+        __asm__ __volatile__("sfence" ::: "memory");
+    }
+
+    /**
+     * This method creates a serializing operation on all load-from-memory and
+     * store-to-memory instructions that were issued prior the mfence
+     * instruction. This serializing operation guarantees that every load and
+     * store instruction that precedes the mfence instruction in program order
+     * becomes globally visible before any load or store instruction that
+     * follows the mfence instruction. 
+     */
+    static void inline mfence() {
+        __asm__ __volatile__("mfence" ::: "memory");
+    }
+};
 
 
 class TSCNS {
@@ -160,22 +214,22 @@ extern uint64_t midnight_ns;
 class StagingBuffer {
 public:
     /**
-             * Attempt to reserve contiguous space for the producer without
-             * making it visible to the consumer. The caller should invoke
-             * finishReservation() before invoking reserveProducerSpace()
-             * again to make the bytes reserved visible to the consumer.
-             *
-             * This mechanism is in place to allow the producer to initialize
-             * the contents of the reservation before exposing it to the
-             * consumer. This function will block behind the consumer if
-             * there's not enough space.
-             *
-             * \param nbytes
-             *      Number of bytes to allocate
-             *
-             * \return
-             *      Pointer to at least nbytes of contiguous space
-             */
+     * Attempt to reserve contiguous space for the producer without
+     * making it visible to the consumer. The caller should invoke
+     * finishReservation() before invoking reserveProducerSpace()
+     * again to make the bytes reserved visible to the consumer.
+     *
+     * This mechanism is in place to allow the producer to initialize
+     * the contents of the reservation before exposing it to the
+     * consumer. This function will block behind the consumer if
+     * there's not enough space.
+     *
+     * \param nbytes
+     *      Number of bytes to allocate
+     *
+     * \return
+     *      Pointer to at least nbytes of contiguous space
+     */
     inline char* reserveProducerSpace(size_t nbytes) {
         ++numAllocations;
 
@@ -187,7 +241,29 @@ public:
         return reserveSpaceInternal(nbytes);
     }
 
-    inline void finishReservation() { varq.push(); }
+    /**
+     * Complement to reserveProducerSpace that makes nbytes starting
+     * from the return of reserveProducerSpace visible to the consumer.
+     *
+     * \param nbytes
+     *      Number of bytes to expose to the consumer
+     */
+    inline void finishReservation() { 
+        assert(nbytes < minFreeSpace);
+        assert(producerPos + nbytes < storage + STAGING_BUFFER_SIZE);
+
+        Fence::sfence(); // Ensures producer finishes writes before bump
+        minFreeSpace -= nbytes;
+        producerPos += nbytes;
+        // producerPos += nbytes is divided into three steps, 
+        // each of which is atomic operation: 
+        // 1) read producerPos
+        // 2) add nbytes to producerPos
+        // 3) store the new value to producerPos
+        // the brand new producerPos is visible to consumer thread 
+        // only when the third step is done, otherwise the stale value
+        // is used. 
+    }
 
     MsgHeader* peek() { return varq.front(); }
 
@@ -207,23 +283,30 @@ public:
     bool checkCanDelete() { return shouldDeallocate; }
 
     StagingBuffer()
-        : varq()
+        : producerPos(storage)
+        , endOfRecordedSpace(storage + STAGING_BUFFER_SIZE)
+        , minFreeSpace(STAGING_BUFFER_SIZE)
+        , cacheLineSpacer()
+        , consumerPos(storage)
+        , shouldDeallocate(false)
+        , storage()
         , shouldDeallocate(false) {
         // Empty function, but causes the C++ runtime to instantiate the
         // sbc thread_local (see documentation in function).
         sbc.stagingBufferCreated();
 
+#ifdef _WIN32
+        unsigned long tid = static_cast<unsigned long>(GetCurrentThreadId());
+        snprintf(name, sizeof(name), "%lu", tid);
+#else
         uint32_t tid = static_cast<pid_t>(::syscall(SYS_gettid));
-        snprintf(name, sizeof(name), "%d", tid);
+        snprintf(name, sizeof(name), "%u", tid);
+#endif
     }
 
     ~StagingBuffer() {}
 
 private:
-    SPSCVarQueueOPT<1 << 20> varq;
-
-    bool shouldDeallocate;
-
     char name[16] = { 0 };
 
     char* reserveSpaceInternal(size_t nbytes, bool blocking = true);
@@ -239,6 +322,11 @@ private:
     // rolling over the producerPos or stalling behind the consumer
     uint64_t minFreeSpace;
 
+    // An extra cache-line to separate the variables that are primarily
+    // updated/read by the producer (above) from the ones by the
+    // consumer(below)
+    char cacheLineSpacer[BYTES_PER_CACHE_LINE];
+
     // Position within the storage buffer where the consumer will consume
     // the next bytes from. This value is only updated by the consumer.
     char* volatile consumerPos;
@@ -249,18 +337,14 @@ private:
     // compression thread.
     bool shouldDeallocate;
 
-    // Uniquely identifies this StagingBuffer for this execution. It's
-    // similar to ThreadId, but is only assigned to threads that NANO_LOG).
-    uint32_t id;
-
     // Backing store used to implement the circular queue
     char storage[STAGING_BUFFER_SIZE];
 
-    StagingBuffer(const StagingBuffer&) = delete;
-    StagingBuffer& operator=(const StagingBuffer&) = delete;
-
     friend RuntimeLogger;
     friend StagingBufferDestroyer;
+
+    StagingBuffer(const StagingBuffer&) = delete;
+    StagingBuffer& operator=(const StagingBuffer&) = delete;
 };
 
 #endif /* RUNTIME_LOGGER_H_ */
